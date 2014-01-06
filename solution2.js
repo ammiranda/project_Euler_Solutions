7a8c140398a87b7210743cc9a0e59c80f493367d
Alexander Miranda <alexandermichaelmiranda@gmail.com>
2014-01-05 23:31:35 -0800

    finished solution to problem 2

--- solution2.js
+++ solution2.js
@@ -1,16 +1,16 @@
-var x = 1;
+var x = 1;  // Initialize both x and y to the initial values of the Fibonacci sequence
 var y = 2;
-var sum = 0;
+var sum = 2; // Initialize the sum variable to two because the initial value is not added by the while loop
 
-for (var y; y < 4000000; x) {
+while (y < 4000000) { // Iterates through the Fibonacci sequence just before the term of value 4000000
 	x += y;
 	y += x;
-	if (x % 2 == 0) {
-		sum += x;
+	if (x % 2 == 0) {  // Check to see if the x term is an even number
+		sum += x;  // Adds even x term to sum variable
 	}
-	if (y % 2 == 0) {
-		sum += y;
+	if (y % 2 == 0) {  // Check to see if the y term is an even number
+		sum += y;  // Adds even y term to sum variable
 	}
 }
 
-console.log(sum);
+console.log(sum); // Outputs sum after the while loop
